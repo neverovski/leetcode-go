2c217f9de257f14f9aedaabb4123ba06bbc38004
Dmitry Neverovski <dmitryneverovski@gmail.com>
2024-03-02 22:55:57 +0100

    feat: solved problem #58

---
+++ 058/solution_test.go
@@ -0,0 +1,16 @@
+package solution_58
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLengthOfLastWord(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal(5, lengthOfLastWord("Hello World"))
+	a.Equal(4, lengthOfLastWord("   fly me   to   the moon  "))
+	a.Equal(6, lengthOfLastWord("luffy is still joyboy"))
+	a.Equal(1, lengthOfLastWord("a"))
+}
